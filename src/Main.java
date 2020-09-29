581eeaa04b695f4afc2e7f19a06f6d779e132afd
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-29 14:07:40 +0800

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // 创建数组；获得数组的长度；
-        int[] myArray = {2, 3, 4, 10, 40};
+        int[] myArray = {2, 13, 560, 1002, 85663};   // 创建数组；获得数组的长度；
         int lengthOfMyArray = myArray.length;
         System.out.println("The length of my array is " + lengthOfMyArray);
 
-        // 查找的目标数
-        int targetNumber = 40;
+        int targetNumber = 560;  // 查找的目标数
 
         Algorithm myAlgorithm = new Algorithm();
+
         int IndexOfTargetNumber = myAlgorithm.BinarySearch(myArray, 0, lengthOfMyArray - 1, targetNumber);
+
         if (IndexOfTargetNumber == -1)
             System.out.println("Target number is not in the array.");
         else

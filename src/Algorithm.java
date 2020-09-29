9ad1057060b683cf166074becb7594051b2a5a2c
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-29 15:37:42 +0800

    updated

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,22 +1,22 @@
 class Algorithm {
-    int NumberOfSearches = 0;
+    int NumberOfSearchesPerformed = 0; // 记录搜索的次数
 
     int BinarySearch(int[] myArray, int IndexOfMinNumber, int IndexOfMaxNumber, int targetNumber) {
-        NumberOfSearches++;
+        NumberOfSearchesPerformed++;
             if (IndexOfMaxNumber >= IndexOfMinNumber) { // 16 >= 0
             int IndexOfMidNumber = IndexOfMinNumber + (IndexOfMaxNumber - IndexOfMinNumber) / 2;
 
-            if (myArray[IndexOfMidNumber] == targetNumber)
-
+            if (myArray[IndexOfMidNumber] == targetNumber){
+                System.out.println("Number Of Searches: " + NumberOfSearchesPerformed);
                 return IndexOfMidNumber;
+            }
 
             if (myArray[IndexOfMidNumber] > targetNumber) // 再次调用BinarySearch方法
                 return BinarySearch(myArray, IndexOfMinNumber, IndexOfMidNumber - 1, targetNumber);
 
-            System.out.println("Time Complexity: " + NumberOfSearches);
+            System.out.println("Number Of Searches Performed: " + NumberOfSearchesPerformed);
             return BinarySearch(myArray, IndexOfMidNumber + 1, IndexOfMaxNumber, targetNumber);
         }
-
         return -1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71}; // 创建一个数组；获得数组的长度；
+        //int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71}; // 创建一个数组；获得数组的长度；
+        int[] myArray = {1, 3, 4, 6, 7}; // 创建一个数组；获得数组的长度；
         int lengthOfMyArray = myArray.length;
         System.out.println("The length of my array is " + lengthOfMyArray);
 
-        int targetNumber = 7; // 查找的目标:数字7
+        int targetNumber = 4; // 查找的目标:数字7
 
         Algorithm myAlgorithm = new Algorithm(); // 实例化出来一个对象myAlgorithm
         int IndexOfTargetNumber = myAlgorithm.BinarySearch(myArray, 0, lengthOfMyArray - 1, targetNumber);

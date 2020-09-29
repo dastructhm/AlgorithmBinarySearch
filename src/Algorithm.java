117f99ba8c9c6677659946496f973fad253a3fb2
bjdhjy888 <bjdhjy678@gmail.com>
2020-09-29 16:07:32 +0800

    updated

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -6,17 +6,20 @@ class Algorithm {
 
         if (IndexOfMaxNumber >= IndexOfMinNumber) { // the remaining half Not empty; 16>=0; 7>=0;
             int IndexOfMidNumber = IndexOfMinNumber + (IndexOfMaxNumber - IndexOfMinNumber) / 2; // 8;
-            System.out.println("Index of Middle Number: " + IndexOfMidNumber);
+            System.out.println("Index of Middle Number: " + IndexOfMidNumber); // 显示中间值的索引
+
             if (myArray[IndexOfMidNumber] == targetNumber){ // 找到目标数字
                 System.out.println("Number Of Searches Performed: " + NumberOfSearchesPerformed);
                 return IndexOfMidNumber; // 返回目标数字的index
             }
 
             if (myArray[IndexOfMidNumber] > targetNumber)
-                // 再次调用BinarySearch方法; 中间值的索引-1，成为新的最大值的索引
+                // 再次调用BinarySearch方法; 中间值的索引-1，成为新的最大值的索引；最小值的索引不变
                 return BinarySearch(myArray, IndexOfMinNumber, IndexOfMidNumber - 1, targetNumber);
 
             System.out.println("Number Of Searches Performed: " + NumberOfSearchesPerformed);
+
+            // 再次调用BinarySearch方法; 中间值的索引+1，成为新的最小值的索引；最大值的索引不变
             return BinarySearch(myArray, IndexOfMidNumber + 1, IndexOfMaxNumber, targetNumber);
         }
         else

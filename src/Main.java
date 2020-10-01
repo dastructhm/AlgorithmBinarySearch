de58388b2170cad0c649ef9e465c8e858545ac92
Bob <41116033+bjdhjy888@users.noreply.github.com>
2020-10-01 10:41:40 +0800

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71}; // 创建一个有17个元素的数组
-        int lengthOfMyArray = myArray.length; // 获得数组的长度；
+
+        // 创建一个有17个元素的数组
+        int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71};
+
+        // 获得数组的长度
+        int lengthOfMyArray = myArray.length;
         System.out.println("The length of my array is " + lengthOfMyArray);
 
-        int targetNumber = 7; // 查找的目标:数字7
+        // 获得数组的长度
+        int targetNumber = 7;
 
-        Algorithm myAlgorithm = new Algorithm(); // 实例化出来一个对象myAlgorithm
+        // 实例化出来一个对象
+        Algorithm myAlgorithm = new Algorithm();
         int IndexOfTargetNumber = myAlgorithm.BinarySearch(myArray, 0, lengthOfMyArray - 1, targetNumber);
 
         if (IndexOfTargetNumber == 0)

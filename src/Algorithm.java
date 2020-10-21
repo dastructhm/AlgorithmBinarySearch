9acddc3a5ba31b177a609607477bafcb8eb174dc
bjdhjy888 <bjdhjy678@gmail.com>
2020-10-21 13:12:02 +0800

    updated

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,19 +1,27 @@
 class Algorithm {
-    int NumberOfSearchesPerformed = 0; // 记录搜索的次数
+    // 记录搜索的次数
+    int NumberOfSearchesPerformed = 0;
 
     int BinarySearch(int[] myArray, int LeftIndex, int RightIndex, int targetValue) {
-        NumberOfSearchesPerformed++; // 调用了一次方法，搜索了一次，所以NumberOfSearchesPerformed + 1
+        // 调用了一次方法，搜索了一次，所以NumberOfSearchesPerformed + 1
+        NumberOfSearchesPerformed++;
 
-        if (RightIndex >= LeftIndex) { // the remaining half Not empty; LeftIndex和RightIndex不重合
-            int MidIndex = LeftIndex + (RightIndex - LeftIndex) / 2; // 注意不要写成(LeftIndex-RightIndex) / 2
-            System.out.println("Current Middle Index: " + MidIndex); // 打印中间值的索引
+        // the remaining half Not empty; LeftIndex和RightIndex不重合
+        if (RightIndex >= LeftIndex) {
+            // 注意不要写成(LeftIndex-RightIndex) / 2
+            int MidIndex = LeftIndex + (RightIndex - LeftIndex) / 2;
+            // 打印中间值的索引
+            System.out.println("Current Middle Index: " + MidIndex);
 
-            if (myArray[MidIndex] == targetValue){ // 找到目标数字
+            // 找到目标数字
+            if (myArray[MidIndex] == targetValue){
                 System.out.println("Number Of Searches Performed: " + NumberOfSearchesPerformed);
-                return MidIndex; // 返回目标数字的index
+                // 返回目标数字的index
+                return MidIndex;
             }
 
-            if (myArray[MidIndex] > targetValue) { // 目标值在中间值左边
+            // 目标值在中间值左边
+            if (myArray[MidIndex] > targetValue) {
                 System.out.print("After Search "  + NumberOfSearchesPerformed + ", current Right Index: " + (MidIndex - 1) + " ");
                 // 再次调用BinarySearch方法; 中间值的索引-1，成为新的最大值的索引；最小值的索引不变
                 return BinarySearch(myArray, LeftIndex, MidIndex - 1, targetValue); // -1是因为MidIndex不是目标的Index
@@ -24,7 +32,9 @@ class Algorithm {
             // 再次调用BinarySearch方法; 中间值的索引+1，成为新的最小值的索引；最大值的索引不变
             return BinarySearch(myArray, MidIndex + 1, RightIndex, targetValue);
         }
-        else // the remaining half is now empty; LeftIndex和RightIndex重合
-            return 0; // 查找结束，且没有返回IndexOfMidNumber,也就是目标数字不在数组中
+        // the remaining half is now empty; LeftIndex和RightIndex重合
+        else
+            // 查找结束，且没有返回IndexOfMidNumber,也就是目标数字不在数组中
+            return 0;
     }
 }

02913a557ffd6c6e0dfc4ba7c81f4a441c01c735
bjdhjy888 <bjdhjy678@gmail.com>
2020-10-21 16:52:24 +0800

    updated

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,3 +1,6 @@
+// int[] myArray = {6, 7, 8, 9, 10};
+// int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71};
+
 class Algorithm {
     // 记录搜索的次数
     int NumberOfSearchesPerformed = 0;
--- src/SetArrayElements.java
+++ src/SetArrayElements.java
@@ -1,5 +1,3 @@
-// int[] myArray = {1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71};
-
 import java.util.Scanner;
 
 public class SetArrayElements {

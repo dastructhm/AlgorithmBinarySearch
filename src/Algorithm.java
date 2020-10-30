b41b1e1fe440d31d6146f750f985a268d4f3a46d
bjdhjy888 <bjdhjy678@gmail.com>
2020-10-30 15:21:09 +0800

    new

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,19 +1,24 @@
 class Algorithm {
-    // 记录搜索的次数
+    // 记录查找的次数
     int SearchesPerformed = 1;
 
+    // 左界下标LeftIndex = 0, 右界下标RightIndex = 数组长度 - 1
     int BinarySearch(int[] myArray, int LeftIndex, int RightIndex, int targetValue) {
-
-        // 左界和右界不重合
         if (LeftIndex <= RightIndex) {
+            // 计算中点下标
             int MidIndex = (LeftIndex + RightIndex) / 2;
-            System.out.println("Search " + SearchesPerformed + ": " + "Left = " + LeftIndex + " Middle = " + MidIndex + " Right = " + RightIndex);
 
+            System.out.println("Search " + SearchesPerformed + ": " + "Left = " +
+                               LeftIndex + " Middle = " + MidIndex + " Right = " + RightIndex);
+
+            // 中点值等于目标值
             if (myArray[MidIndex] == targetValue){
                 System.out.println("Searches Performed: " + SearchesPerformed);
+                // 返回中点下标
                 return MidIndex;
             }
 
+            // 目标值在中点左边
             else if (myArray[MidIndex] > targetValue) {
                 SearchesPerformed++;
                 // 递归：BinarySearch调用BinarySearch自身
@@ -21,10 +26,10 @@ class Algorithm {
             }
 
             SearchesPerformed++;
-            // 递归：BinarySearch调用BinarySearch自身
+            // 目标值在中点右边
             return BinarySearch(myArray, MidIndex + 1, RightIndex, targetValue);
         }
-        // 左界和右界重合，且数组中没有目标值
+        // 左界和右界重合，中点值不等于目标值
         else
             return -1;
     }
